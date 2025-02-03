294d644e0887624234a736f431f87d0379bdf894
Moisés Zambrano <moiseszambrano488@gmail.com>
2025-02-03 18:28:32 -0400

    config

--- vite.config.ts
+++ vite.config.ts
@@ -1,14 +1,22 @@
-import { defineConfig } from 'vite'
-import tailwindcss from '@tailwindcss/vite'
-import react from '@vitejs/plugin-react-swc'
-import path from 'path'
+import { defineConfig } from "vite";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react-swc";
+import path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, "index.html"),
+      },
+    },
+    assetsDir: "assets",
+  },
   resolve: {
     alias: {
-      "@": path.resolve (__dirname, "./src" )
-    }
-  }
-})
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
+});
